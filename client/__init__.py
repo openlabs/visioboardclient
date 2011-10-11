36272ee98853a38b9576c7a625fe9400fa5990e6
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-10-11 10:51:47 -0300

    Improved data

--- client/__init__.py
+++ client/__init__.py
@@ -99,12 +99,68 @@ class GeoChart(Data):
     def add_column(self, id, label, type):
         """Add a column
         """
-        self.options['cols'].append({'id': id, 'label': label, 'type': type})
+        return self.options['cols'].append(
+            {'id': id, 'label': label, 'type': type}
+            )
 
     def add_row(self, values):
         """Adds a row
         """
-        self.series.append({'c': [{'v': v} for v in values]})
+        return self.series.append({'c': [{'v': v} for v in values]})
+
+
+class Table(Data):
+
+    def __init__(self):
+        super(Table, self).__init__()
+        self.options['cols'] = []
+
+    def add_column(self, id, label):
+        return self.options['cols'].append({'id': id, 'label': label})
+
+    def add_row(self, values):
+        return self.series.append({'c': values})
+
+
+class Graph(Data):
+    
+    def __init__(self):
+        super(Graph, self).__init__()
+
+    def add_series(self, type, name, data):
+        """
+        :param type: column, pie etc
+        :param name: name of the graph
+        :param data: a list of values corresponding to graph
+                     ex: {'name': 'Customer', 'y': 100}
+        """
+        return self.series.append({
+            'type': type,
+            'name': name,
+            'data': data,
+            })
+    def set_option(self, option, value):
+        self.options[option] = value
+
+    def set_x_axis(self, **kwargs):
+        """View options here: http://www.highcharts.com/ref/#xAxis
+        """
+        self.set_option('xAxis', kwargs)
+
+    def set_y_axis(self, **kwargs):
+        """View options here: http://www.highcharts.com/ref/#yAxis
+        """
+        self.set_option('yAxis', kwargs)
+
+    def set_tooltip(self, **kwargs):
+        """Set tooltip options: http://www.highcharts.com/ref/#tooltip
+        """
+        self.set_option('tooltip', kwargs)
+
+    def set_credits(self, **kwargs):
+        """Set the credits: http://www.highcharts.com/ref/#credits
+        """
+        self.set_option('credits', kwargs)
 
 
 class Client(object):
@@ -127,8 +183,8 @@ class Client(object):
         """
         pass
 
-    def geo_chart(self, widget, data):
-        """Sends data to a Geo Chart widget on dashboard
+    def send_data(self, widget, data):
+        """Sends data to a widget on dashboard
         """
         if isinstance(data, Data):
             data = data.data_dict
